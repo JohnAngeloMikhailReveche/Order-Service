fd7da24d24176f2373a262921cf7c4593eddef13
JohnAngeloMikhailReveche2 <johnangelomikhailareveche@iskolarngbayan.pup.edu.ph>
2025-12-22 15:09:10 +0800

    Made a new migration called Updated-Cart-Item-Table

--- Order-Microservice/OrderService/Migrations/20251222070740_Updated-Cart-Item-Table.cs
+++ Order-Microservice/OrderService/Migrations/20251222070740_Updated-Cart-Item-Table.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace OrderService.Migrations
 {
     /// <inheritdoc />
-    public partial class CreateCartItemTable : Migration
+    public partial class UpdatedCartItemTable : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
