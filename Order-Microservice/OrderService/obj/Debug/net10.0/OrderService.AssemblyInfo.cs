0088c51e2278023ae4f4f870aeaa9fbfaa2c305d
JohnAngeloMikhailReveche2 <johnangelomikhailareveche@iskolarngbayan.pup.edu.ph>
2025-12-23 12:04:29 +0800

    Added the Order Feedback Model to the EFCore

--- Order-Microservice/OrderService/obj/Debug/net10.0/OrderService.AssemblyInfo.cs
+++ Order-Microservice/OrderService/obj/Debug/net10.0/OrderService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ec72033a3b440890255563b4e7c3b56e75d84c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aecc2b3ddf1880fe74ddd215f99648533219e861")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
