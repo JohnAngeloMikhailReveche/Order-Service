5cb80b417d322df20809b9266f6dbdf9f42aaacd
JohnAngeloMikhailReveche2 <johnangelomikhailareveche@iskolarngbayan.pup.edu.ph>
2025-12-23 13:11:04 +0800

    Added a OrderFeedbackService for the Feedback System

--- Order-Microservice/OrderService/obj/Debug/net10.0/OrderService.AssemblyInfo.cs
+++ Order-Microservice/OrderService/obj/Debug/net10.0/OrderService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0088c51e2278023ae4f4f870aeaa9fbfaa2c305d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d55edd655c5cd6b05f085b99c1a4f2e2bdad0520")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
